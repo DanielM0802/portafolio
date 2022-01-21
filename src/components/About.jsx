4e4dec0b4f8834f887b06eaf4f14e5dccc70c69b
Daniel <marillancadaniel@gmail.com>
2022-01-21 19:53:01 -0300

    correcion en about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,8 @@ const About = () => {
                 <h3 className="text-5xl font-bold mb-10">Sobre mí</h3>
                 <div className="descripcion">
                     <p>¡Hola! Mi nombre es Daniel Marillanca y tengo 19 años. Actualmente me encuentro cursando el 3 año de la carrera de Ingeniería Informática en la Universidad de la Frontera.</p>
-                    <p>Mi principal objetivo es poner en práctica las tecnologías que he ido aprendiendo tanto en mi carrera como de forma autodidácta.</p>
-                    <p>Mi principal objetivo es ganar experiencia en el desarrollo web frontend, para, posteriormente adquirir nuevos conocimientos en el área del backend.</p>
-                    <p>Tengo una gran pasion por el desarrollo de software, siempre trato de mejorar cada dia, aprendiendo nuevas habilidades y poniendolas en práctica mediante proyectos personales.
+                    <p>Mi principal objetivo es poner en práctica las tecnologías que he ido aprendiendo tanto en mi carrera como de forma autodidacta.</p>
+                    <p>Tengo una gran pasión por el desarrollo de software, siempre trato de mejorar cada día aprendiendo nuevas habilidades y poniendolas en práctica mediante proyectos personales.
                     </p>
                 </div>
                 <a href="https://www.dropbox.com/s/kv4odq3zh223fzi/Marillanca_Daniel_CV.pdf?dl=1" target={"_blank"}>
